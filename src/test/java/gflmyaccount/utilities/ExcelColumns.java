6e0d9479127be97b7cb1c95f3ed8345ac46376b7
krinaparmar <65011387+krinaparmar@users.noreply.github.com>
2020-11-30 17:38:03 -0500

    Revert "Krina myaccount"

---
+++ src/test/java/gflmyaccount/utilities/ExcelColumns.java
@@ -0,0 +1,25 @@
+package gflmyaccount.utilities;
+
+public interface ExcelColumns {
+
+    int FIRSTNAME = 0;
+    int LASTNAME = 1;
+    int EMAIL = 2;
+    int PWD= 3;
+    int RE_ENTER_PASSWORD = 4;
+    int SECURITYQUESTIONS= 5;
+    int SECURITYANSWER = 6;
+    int PHONE = 7;
+    int EXTENSION = 8;
+    int MOBILE = 9;
+    int FAX = 10;
+    int LANGUAGE= 11;
+    int DRIVER_NOTES = 12;
+    int CUSTOMER_ID = 13;
+    int BRANCH_BU = 14;
+    int SCALE_TICKET = 15;
+    int WEIGHT = 16;
+    int PAYMENT = 17;
+    int CONTAINER_NAME = 0;
+    int STORAGE_NAME = 0;
+}
